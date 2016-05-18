b913b2f3851fceaaa6d649aee02a68dde1548cda
Paul Seyfert <paul.seyfert@mib.infn.it>
2016-05-18 10:47:05 +0200

    hack systems with broken ccache call from gInterpreter

--- module/tmva_branch_adder.py
+++ module/tmva_branch_adder.py
@@ -1,4 +1,23 @@
 from ROOT import gInterpreter
+def hack_compilation():
+    from ROOT import gSystem
+    cmd = gSystem.GetMakeSharedLib()
+    import subprocess
+    compiler = subprocess.check_output(["root-config","--cxx"])
+    compiler=compiler[:-1] # remove trailing line feed
+    if compiler.find(" "):
+        print "WILL NEED TO FIX BUILD COMMAND"
+        print "old command:"
+        print cmd
+        # the GetMakeSharedLib is broken on one of my systems because the
+        # compiler command contains a white space "ccache g++"
+        # gInterpreter then uses only the first word "ccache"
+        cmd = cmd.replace("ccache",compiler)
+        gSystem.SetMakeSharedLib(cmd)
+        print "new command:"
+        print cmd
+
+hack_compilation()
 gInterpreter.LoadMacro("../src/blacklist.cpp+")
 gInterpreter.LoadMacro("../src/reader_wrapper.cpp+")
 from ROOT import reader_wrapper
